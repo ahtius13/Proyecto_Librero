fdcff6ea9b7d4c8743fd2675634683cd4b1db370
Boolean-type <sergio23.tp@gmail.com>
2026-01-18 21:42:12 +0100

    Update test_usuarios.py

--- tests/test_usuarios.py
+++ tests/test_usuarios.py
@@ -1,23 +1,6 @@
 import pytest
 
 
-@pytest.fixture
-def gestor_usuarios():
-    return GestorUsuarios()
-
-
-@pytest.fixture
-def usuario():
-    return Usuario(
-        codigo="U001",
-        nombre="Ana",
-        apellido="López",
-        tipo="socio",
-        direccion="Calle Mayor",
-        telefono="123456789"
-    )
-
-
 def test_codigo_socio_unico(gestor_usuarios, usuario):
     pass
 
