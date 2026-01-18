ac745f49d07d0454bef941bc500d28e26223d127
Boolean-type <sergio23.tp@gmail.com>
2026-01-18 21:48:22 +0100

    Update test_ventas.py

--- tests/test_ventas.py
+++ tests/test_ventas.py
@@ -5,9 +5,6 @@ def test_descuento_solo_para_socios(gestor_ventas):
     pass
 
 
-def test_no_descuento_no_socio(gestor_ventas):
-    pass
-
 
 def test_stock_se_reduce_tras_venta(gestor_ventas):
     pass
