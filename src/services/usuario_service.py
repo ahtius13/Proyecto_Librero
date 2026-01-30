4dc68cae40cb085a97281dae1339dc5cc2a1e116
RubenCano <erreceeme20@gmail.com>
2026-01-30 13:40:39 +0100

    modificando usuario metodos crud eliminar, modificar y crear para hacerlo mas modular y eficiente

--- src/services/usuario_service.py
+++ src/services/usuario_service.py
@@ -4,14 +4,20 @@ from exceptions import usuarioDuplicado, usuarioInexistente
 class usuario_service:
     def __init__(self):
         self.data =JsonFunctions("data/usuarios.json")
-        
-    def crearUsuario(self, usuario:Usuario):
-        UsuariosJson: list=self.data.get_all()
-        
-        if usuario.idUsuario in [diccionarioUsuario.get("IdUsuario") for diccionarioUsuario in UsuariosJson]:
-            raise usuarioDuplicado("Ya existe un usuario")
+        self.usuariosJson: list=self.data.get_all()
+
+    def __verificarUsuarioExistente(id, listaUsuario:list):
+        #Evalua si existe el usuario en la lista indicada
+        indice=""
+        listaId=[usuario.get("IdUsuario") for usuario in listaUsuario]
+        if id in listaId:
+            indice=listaId.index(id)
         else:
-            UsuariosJson.append({
+            raise usuarioInexistente(f"No hay ningun usuario con la id {id}")
+        return indice
+    
+    def __fromUsuarioToDict(usuario:Usuario)->dict:
+        diccionarioUsuario={
                 "IdUsuario":usuario.idUsuario,
 
                 "Nombre":usuario.nombre,
@@ -23,26 +29,33 @@ class usuario_service:
                 "Dirección":usuario.direccion,
 
                 "Teléfono":usuario.telefono
-            })
-            self.data.save_all(UsuariosJson)
+            }
+        return diccionarioUsuario
+        
+    def crearUsuario(self, usuario:Usuario):
+        
+        if usuario.idUsuario in [diccionarioUsuario.get("IdUsuario") for diccionarioUsuario in self.usuariosJson]:
+            raise usuarioDuplicado("Ya existe un usuario")
+        else:
+            self.usuariosJson.append(self.__fromUsuarioToDict(usuario))
+
+            self.data.save_all(self.usuariosJson)
             
 
-    def modificarUsuario(self, id, usuario:Usuario):
-        #necesito sacar el indice que representa el diccionario dentro de la list de diccionarios que es el json, para modificarlo.
-        UsuariosJson: list=self.data.get_all()
-        usuarioJson= next((usuario for usuario in UsuariosJson if usuario.get("IdUsuario")==id),None)
-        if not usuarioJson:
-            raise usuarioInexistente(f"No hay ningun usuario con la id {id}")
-        else:
-            UsuariosJson[id-1]
+    def modificarUsuario(self, usuario:Usuario):
+        indice=self.__verificarUsuarioExistente(usuario.idUsuario, self.usuariosJson)
+
+        self.usuariosJson[indice]=self.__fromUsuarioToDict(usuario)
+
+        self.data.save_all(self.usuariosJson)
+        
 
     def eliminarUsuario(self, id):
-        UsuariosJson: list=self.data.get_all()
-        usuarioJson= next((usuario for usuario in UsuariosJson if usuario.get("IdUsuario")==id),None)
-        if not usuarioJson:
-            raise usuarioInexistente(f"No hay ningun usuario con la id {id}")
-        else:
-            UsuariosJson.remove(usuarioJson)
+        indice=self.__verificarUsuarioExistente(id, self.usuariosJson)
+
+        self.usuariosJson.pop(indice)
+
+        self.data.save_all(self.usuariosJson)
             
 
     def consultarUsuario(self, id):
