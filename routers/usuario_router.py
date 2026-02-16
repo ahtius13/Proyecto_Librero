7849b1c8b4d7feb81330b1a37d955e4369f8f55b
RubenCano <erreceeme20@gmail.com>
2026-02-16 12:45:11 +0100

    terminado Update y Delete

--- routers/usuario_router.py
+++ routers/usuario_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from schemas. usuario_model import Usuario_model
 from src.usuarios import Usuario, UsuarioManager
 from exceptions.recursoYaExistenteException import RecursoYaExistenteException
@@ -14,7 +14,9 @@ def crear_usuario(usuario_model:Usuario_model) :
     usuario_manager=UsuarioManager()
     usuario=Usuario(usuario_model.nombre, usuario_model.apellido,usuario_model.numero_socio, usuario_model.tipo, usuario_model.direccion, usuario_model.telefono)
     try:
+        #Comprobando si no existe con try
         usuario_manager.anadir_usuario(usuario)
+
         return {"message":f"el usuario con identificador {usuario.numero_socio} ha sido creado"}
     except ValueError():
         raise RecursoYaExistenteException("usuario",usuario.numero_socio)
@@ -32,17 +34,37 @@ def ver_usuario(numSocio) :
     #ver informacion sobre usuario con el numSocio indicado.
     usuario_manager=UsuarioManager()
     try:
+        #Comprobando si existe con try
         usuario=usuario_manager.consultar_usuario(numSocio)
+
         return usuario.to_dict()
     except ValueError:
         raise RecursoInexistenteException("usuario", numSocio)
 
-@router.put("/{numSocio}")
-def modificar_usuario(numSocio, data) :
+@router.put("/{numSocio}", codigoEstado=status.HTTP_200_OK)
+def modificar_usuario(numSocio, usuario_model:Usuario_model) :
     #modifica usuario con el numsocio indicado con el contenido indicado en el JSON
-    pass
+    usuario_manager=UsuarioManager()
+    try:
+        #Comprobando si existe con try
+        usuario_manager.consultar_usuario(numSocio)
 
-@router.delete("/{numSocio}")
+        if numSocio != usuario_model.numero_socio:
+            raise HTTPException(400, "no se puede cambiar el numero de socio de un usuario")
+        usuario_manager.modificar_usuario(numSocio, usuario_model.nombre, usuario_model.apellido,usuario_model.numero_socio, usuario_model.tipo, usuario_model.direccion, usuario_model.telefono)
+    except ValueError:
+        raise RecursoInexistenteException("usuario", numSocio)
+    
+@router.delete("/{numSocio}", codigoEstado=status.HTTP_200_OK)
 def eliminar_usuario(numSocio) :
     #elimina usuario con el numSocio indicado.
-    pass
+    usuario_manager=UsuarioManager()
+    
+    usuario_manager=UsuarioManager()
+    try:
+        #Comprobando si existe con try
+        usuario_manager.consultar_usuario(numSocio)
+
+        usuario_manager.eliminar_usuario()
+    except ValueError:
+        raise RecursoInexistenteException("usuario", numSocio)
